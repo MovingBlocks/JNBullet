de1db075240180eec693bb6b27259ce086882845
Michael <polli104@mail.chapman.edu>
2019-07-01 21:15:10 -0700

    added cmake build for natives

--- README.md
+++ README.md
@@ -1,13 +1,75 @@
 # TeraBullet (Terasology Version)
 
-TeraBullet is a version of bullet with extensions for direct interactions for voxel worlds. Written for use with Terasology. 
-this is an implementation of bullet that wraps around native bullet using SWIG.
+TeraBullet is a version of bullet with extensions for direct interactions for voxel worlds. Written for use with Terasology. this is an implementation of bullet wraps around native bullet using SWIG.
 
 # Building
 
-The java and native side of this process is entirely done through gradle. "./gradlew build" is sufficient to produce both the shared library and jar.
+## CMAKE
+* \<toolchain\>
+  -  android_armeabi_gcc.cmake
+  -  linux_amd64_gcc.cmake
+  -  linux_i386_gcc.cmake
+  -  linux_windows_amd64_mingw32.cmake
+  -  linux_windows_i686_mingw32.cmake
+  -  macosx_amd64_clang.cmake
+  -  windows_amd64_msvc.cmake
+  -  windows_i386_msvc.cmake
+
+
+```
+cd build
+cmake ../ -DCMAKE_TOOLCHAIN_FILE=../toolchains/<toolchain>.cmake
+make
+```
+
+## linux - linux and windows
 
 To build the natives on linux you need:
 
-* swig installed on the system to produce both the generated code for the native and java bindings
-* gcc compiler (gcc-multilib g++-multilib for x86)
+- ant - installed on the system to build the natives for the generated wrapper code
+- swig - installed on the system to produce both the generated code for the native and java bindings
+
+* linux
+  - i686
+    1. gcc
+    2. gcc-c++ 
+    3. libstdc++.i686
+    4. glibc-devel.i686
+  - amd64
+    1. gcc
+    2. gcc-c++ 
+    3. glibc-devel
+    4. libstdc++
+* windows
+  - i686
+    1. mingw32-winpthreads
+    2. mingw32-winpthreads-static
+    3. mingw32-gcc
+    4. mingw32-gcc-c++
+  - amd64
+    1. mingw64-winpthreads
+    2. mingw64-winpthreads-static
+    3. mingw64-gcc
+    4. mingw64-gcc-c++
+
+Then simple run ```ant``` to generated the binding code between java along with compiling the necessary libraries. 
+
+
+the output is produced under the ```native-build``` folder and is formatted in this fashion:
+```libbullet-[linux,windows]-[i686,amd64].[dll,so]```
+
+
+
+To build the java portion of bullet, simply run ```./gralew build```
+
+## OSX
+
+* mac
+  - i686
+  - amd64
+
+
+# License
+
+This library is Licensed under the [Apache 2 License](http://www.apache.org/licenses/LICENSE-2.0.html) and is a rework of bullet wrapper
+from [libgdx](https://github.com/libgdx/libgdx)
---
+++ toolchains/android_armeabi_gcc.cmake
@@ -0,0 +1,120 @@
+# Copyright 2015, alex at staticlibs.net
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# toolchain for linux: https://github.com/staticlibs/android-ndk-r9d-arm-linux-androideabi-4.8
+
+cmake_minimum_required ( VERSION 2.8.12 )
+
+# default to Debug
+set ( CMAKE_BUILD_TYPE "Debug" CACHE STRING "Default build type" )
+
+set ( ANDROID_TOOLCHAIN_NAME arm-linux-androideabi CACHE INTERNAL "" )
+set ( ANDROID_TOOLCHAIN_DIR "SPECIFY_ME_I_AM_ANDROID_TOOLCHAIN_DIR" CACHE STRING "" )
+set ( ANDROID_TOOLCHAIN_PREFIX ${ANDROID_TOOLCHAIN_DIR}/bin/${ANDROID_TOOLCHAIN_NAME} CACHE INTERNAL "" )
+set ( ANDROID_GNUSTL_SHARED_PATH ${ANDROID_TOOLCHAIN_DIR}/${ANDROID_TOOLCHAIN_NAME}/lib/armv7-a/thumb )
+
+set ( CMAKE_SYSTEM_NAME Generic )
+set ( UNIX 1 CACHE INTERNAL "" )
+set ( CMAKE_HOST arm-eabi-linux )
+set ( CMAKE_SYSROOT ${ANDROID_TOOLCHAIN_DIR}/sysroot CACHE INTERNAL "" )
+set ( CMAKE_C_COMPILER ${ANDROID_TOOLCHAIN_PREFIX}-gcc CACHE INTERNAL "" )
+set ( CMAKE_CXX_COMPILER ${ANDROID_TOOLCHAIN_PREFIX}-g++ CACHE INTERNAL "" )
+
+# CMAKE_C_FLAGS
+set ( CMAKE_C_FLAGS_LIST
+        -fPIC
+        -DANDROID
+        -mandroid
+        --sysroot=${CMAKE_SYSROOT}
+        -march=armv7-a
+        -mthumb
+        -mfloat-abi=softfp
+        -mfpu=vfpv3-d16 )
+set ( CMAKE_C_FLAGS "${CMAKE_C_FLAGS}  " )
+string ( REPLACE ";" " " CMAKE_C_FLAGS "${CMAKE_C_FLAGS_LIST}" )
+set ( CMAKE_C_FLAGS "${CMAKE_C_FLAGS}" CACHE INTERNAL "" )
+
+# CMAKE_C_FLAGS_DEBUG
+set ( CMAKE_C_FLAGS_DEBUG_LIST
+        -g
+        -O0 )
+string ( REPLACE ";" " " CMAKE_C_FLAGS_DEBUG "${CMAKE_C_FLAGS_DEBUG_LIST}" )
+set ( CMAKE_C_FLAGS_DEBUG "${CMAKE_C_FLAGS_DEBUG}" CACHE INTERNAL "" )
+
+# CMAKE_C_FLAGS_RELEASE
+set ( CMAKE_C_FLAGS_RELEASE_LIST
+        -Os
+        -DNDEBUG )
+string ( REPLACE ";" " " CMAKE_C_FLAGS_RELEASE "${CMAKE_C_FLAGS_RELEASE_LIST}" )
+set ( CMAKE_C_FLAGS_RELEASE "${CMAKE_C_FLAGS_RELEASE}" CACHE INTERNAL "" )
+
+# STATICLIB_CXX_STANDARD
+if ( NOT DEFINED STATICLIB_CXX_STANDARD )
+    set ( STATICLIB_CXX_STANDARD c++11 CACHE INTERNAL "" )
+endif ( )
+
+# CMAKE_CXX_FLAGS
+set ( CMAKE_CXX_FLAGS_LIST
+        --std=${STATICLIB_CXX_STANDARD}
+        -fPIC
+        -Wall
+        -Werror
+        -Wextra
+        -fno-strict-overflow
+        -fno-strict-aliasing
+        -fstack-protector-all
+        -Wlogical-op
+        -DANDROID
+        -mandroid
+        --sysroot=${CMAKE_SYSROOT}
+        -march=armv7-a
+        -mthumb
+        -mfloat-abi=softfp
+        -mfpu=vfpv3-d16
+)
+string ( REPLACE ";" " " CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS_LIST}" )
+set ( CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS}" CACHE INTERNAL "" )
+
+# CMAKE_CXX_FLAGS_DEBUG
+set ( CMAKE_CXX_FLAGS_DEBUG_LIST
+        -g
+        -O0 )
+string ( REPLACE ";" " " CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG_LIST}" )
+set ( CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG}" CACHE INTERNAL "" )
+
+# CMAKE_CXX_FLAGS_RELEASE
+set ( CMAKE_CXX_FLAGS_RELEASE_LIST
+        -Os
+        -DNDEBUG )
+string ( REPLACE ";" " " CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE_LIST}" )
+set ( CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE}" CACHE INTERNAL "" )
+
+# CMAKE_EXE_LINKER_FLAGS_LIST
+set ( CMAKE_EXE_LINKER_FLAGS_LIST
+        -DANDROID
+        -mandroid
+        -march=armv7-a
+        -mthumb
+        -Wl,--fix-cortex-a8 )
+string ( REPLACE ";" " " CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS_LIST}" )
+set ( CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS}" CACHE INTERNAL "" )
+
+set ( CMAKE_AR ${ANDROID_TOOLCHAIN_PREFIX}-ar CACHE INTERNAL "" )
+set ( CMAKE_AS ${ANDROID_TOOLCHAIN_PREFIX}-as CACHE INTERNAL "" )
+set ( CMAKE_LD ${ANDROID_TOOLCHAIN_PREFIX}-ld CACHE INTERNAL "" )
+set ( CMAKE_NM ${ANDROID_TOOLCHAIN_PREFIX}-nm CACHE INTERNAL "" )
+set ( CMAKE_OBJCOPY ${ANDROID_TOOLCHAIN_PREFIX}-objcopy CACHE INTERNAL "" )
+set ( CMAKE_OBJDUMP ${ANDROID_TOOLCHAIN_PREFIX}-objdump CACHE INTERNAL "" )
+set ( CMAKE_RANLIB ${ANDROID_TOOLCHAIN_PREFIX}-ranlib CACHE INTERNAL "" )
+set ( CMAKE_STRIP ${ANDROID_TOOLCHAIN_PREFIX}-strip CACHE INTERNAL "" )
